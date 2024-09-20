fb6c01be3ab6ecee23cdf9ad7e17355bc878206b
bernoussama <oussama.bernou@outlook.com>
2024-09-20 22:35:22 +0100

    chore - cleanup

--- src/main.c
+++ src/main.c
@@ -249,38 +249,8 @@ int main(void)
 	// Get window surface
 	screenSurface = SDL_GetWindowSurface(window);
 
-	// Fill the surface white
-	// SDL_FillRect(screenSurface, NULL,
-	//              SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-	//
-	// // Update the surface
-	// SDL_UpdateWindowSurface(window);
-
 	SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
 	SDL_RenderClear(renderer);
-
-	// Render red filled quad
-	// SDL_Rect fillRect = {SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4,
-	// SCREEN_WIDTH / 2,
-	//                      SCREEN_HEIGHT / 2};
-	//
-	// SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
-	// SDL_RenderFillRect(renderer, &fillRect);
-	//
-	// // Render green outlined quad
-	// SDL_Rect outlineRect = {SCREEN_WIDTH / 6, SCREEN_HEIGHT / 6,
-	//                         SCREEN_WIDTH * 2 / 3, SCREEN_HEIGHT * 2 / 3};
-	// SDL_SetRenderDrawColor(renderer, 0x00, 0xFF, 0x00, 0xFF);
-	// SDL_RenderDrawRect(renderer, &outlineRect);
-	//
-	// // Render blue line
-	// SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0xFF, 0xFF);
-	// SDL_RenderDrawLine(renderer, 0, SCREEN_HEIGHT / 2, SCREEN_WIDTH,
-	//                    SCREEN_HEIGHT / 2);
-	// SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0x00, 0xFF);
-	// for (int i = 0; i < SCREEN_HEIGHT; i += 4) {
-	//   SDL_RenderDrawPoint(renderer, SCREEN_WIDTH / 2, i);
-	// }
 
 	SDL_Rect player = {posX - 4, posY - 4, 8, 8};
 
